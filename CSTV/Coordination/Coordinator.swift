d0be89c42ac0c8a26594a7937cbc31f9bbed5098
vbrittes <victor.mbrittes@gmail.com>
2025-02-28 10:21:07 -0300

    setup coordinator

---
+++ CSTV/Coordination/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  CSTV
+//
+//  Created by Victor Milen Brittes on 28/02/25.
+//
+
+import UIKit
+
+protocol Coordinator {
+    var navigationController: UINavigationController { get set }
+    func start()
+}
