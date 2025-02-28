d3d5f156706152435490ca68621983d371fed4eb
vbrittes <victor.mbrittes@gmail.com>
2025-02-28 11:46:55 -0300

    create match scene classes

---
+++ CSTV/Scenes/Match List/MatchListTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MatchListTableViewController.swift
+//  CSTV
+//
+//  Created by Victor Milen Brittes on 28/02/25.
+//
+
+import UIKit
+
+class MatchListTableViewController: UITableViewController {
+    
+}
---
+++ CSTV/Scenes/Match List/MatchListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MatchListViewModel.swift
+//  CSTV
+//
+//  Created by Victor Milen Brittes on 28/02/25.
+//
+
+class MatchListViewModel {
+    
+    weak var coordinator: Coordinator?
+    
+}
