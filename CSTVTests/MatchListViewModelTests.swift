1f3b4fa355559c6094f998580c50979a8fcb62ed
vbrittes <victor.mbrittes@gmail.com>
2025-02-28 11:47:08 -0300

    create tests

---
+++ CSTVTests/MatchListViewModelTests.swift
@@ -0,0 +1,24 @@
+//
+//  MatchListViewModelTests.swift
+//  CSTV
+//
+//  Created by Victor Milen Brittes on 28/02/25.
+//
+
+import XCTest
+@testable import CSTV
+
+final class MatchListViewModelTests: XCTestCase {
+    
+    var sut: MatchListViewModel!
+    
+    override func setUp() {
+        sut = MatchListViewModel()
+    }
+    
+    override class func tearDown() {
+        
+    }
+    
+    
+}
